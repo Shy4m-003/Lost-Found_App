19978b244232c4582072eec00044aa5e16df16be
Shy4m-003 <shyamprasadreddyginkala@gmail.com>
2025-10-01 20:15:38 +0530

    Upgraded Spring version to 3.4.6

--- user-service/src/main/java/com/pagasi/lostfound/user_service/repository/contactInfoRepository.java
+++ user-service/src/main/java/com/pagasi/lostfound/user_service/repository/contactInfoRepository.java
@@ -3,5 +3,8 @@ package com.pagasi.lostfound.user_service.repository;
 import com.pagasi.lostfound.user_service.entity.ContactEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface contactInfoRepository extends CrudRepository<ContactEntity, Long> {
+    Optional<ContactEntity> findByUser_Id(Long id);
 }
